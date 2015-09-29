ea9c21aeafeba65ec9665bd1744ab5c74e0a0d06
nigelfloe <dwr229@gmail.com>
2015-09-29 13:31:07 -0400

    squares oriented

--- app/assets/javascripts/square.js
+++ app/assets/javascripts/square.js
@@ -1,4 +1,20 @@
-var Square = function(position){
-  this.position = position;
-  debugger
+var Square = function(color, positionX, positionY){
+  this.color = color;
+  this.positionX = positionX;
+  this.positionY = positionY;
+  Square.all.push(this);
+};
+
+Square.all = [];
+
+Square.findByPosition = function(x, y){
+  return Square.all.filter(function(square){
+    return square.positionX == x && square.positionY == y;
+  })
+
+};
+
+
+Square.prototype.render = function(){
+  return "<div class='" + this.color + " square' position-x='" + this.positionX + "' position-y='" + this.positionY + "'></div>"
 };

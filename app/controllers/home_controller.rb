23104f13019f56dc040888bee8ed75024a16002a
nancyhawa <hawa.nancy@gmail.com>
2015-10-01 17:46:30 -0400

    add user login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if !user_signed_in?
+      redirect_to '/users/sign_in'
+    end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'home#index'
+  root 'home#index' #landing page/sign up
+
+  get '/game', to: 'game#index'
+  # get '/game/new', to: 'game#new'
+  # post '/game', to: 'game#create'
+
+  get '/leaderboard', to: 'user#index'
+
+
 end

a0c5debb6fef414dd44a9d0d88fc6ebb5b21cfd2
nigelfloe <dwr229@gmail.com>
2015-10-08 17:00:06 -0400

    next level in progress

--- app/models/louis.rb
+++ app/models/louis.rb
@@ -33,29 +33,85 @@ class Louis
     end
   end
 
-  def choose(board)
-    louis_choices = choices_array(board.state, LOUIS)
-
-    p1_choices = louis_choices.map do |louis_choice|
-      choices_array(louis_choice.last, P1)
+  def determine_choices(opponent_choices_array, player)
+    opponent_choices_array.map do |opponent_choice|
+      choices_array(opponent_choice.last, player)
     end
-
-    game_tree = Hash[louis_choices.zip(p1_choices)]
-
-    game_tree.each do |louis_move, p1_responses|
-      p1_responses.sort_by! do |response|
-        evaluate_board(response.last, LOUIS)
-      end
-      #select p1's best response
-      game_tree[louis_move] = evaluate_board(p1_responses.first.last, LOUIS)
-    end
-
-
-    best_choices = game_tree.select do |louis_move, outcome_value|
-      outcome_value == game_tree.values.max
-    end
-    best_choices.keys.sample
   end
+
+  def opponent(player)
+    (player - 1).abs
+  end
+
+  def choose(board_state, counter, player, choices_hash)
+    unless counter == 0
+      player_choices = choices_array(board_state, player)
+
+      player_choices.each do |choice|
+        binding.pry
+        opponent_responses = choices_array(choice.last, opponent(player))
+        binding.pry
+        choices_hash[choice] = opponent_responses.map do |response|
+          binding.pry
+          if counter == 1
+            evaluate_board(response, LOUIS)
+          else
+            choose(response, counter-1, opponent(player), choices_hash)
+          end
+        end
+      end
+    end
+    choices_hash
+  end
+
+  # def decide(board)
+  #   choose(board, 4, LOUIS).each do |louis_move, p1_responses|
+  #     p1_responses.sort_by! do |response|
+  #       evaluate_board(response.last, LOUIS)
+  #     end
+  #     #select p1's best response
+  #     game_tree[louis_move] = evaluate_board(p1_responses.first.last, LOUIS)
+  #   end
+  #
+  #
+  #   best_choices = game_tree.select do |louis_move, outcome_value|
+  #     outcome_value == game_tree.values.max
+  #   end
+  #   best_choices.keys.sample
+  # end
+  #
+  # def choose(board)
+  #   louis_choices = choices_array(board.state, LOUIS)
+  #   choices_hash = {}
+  #   louis_choices.each do |choice|
+  #     choices_array(choice.last, )
+  #     choices_hash[choice] = 0
+  #   end
+  #   p1_choices = determine_choices(louis_choices, P1)
+  #   binding.pry
+  #   louis_responses = determine_choices(p1_choices, LOUIS)
+  #
+  #   # p1_choices = louis_choices.map do |louis_choice|
+  #   #   choices_array(louis_choice.last, P1)
+  #   # end
+  #   # louis_responses = p1_choices.map do |p1_choice|
+  #   # end
+  #
+  #   # game_tree = Hash[louis_choices.zip(p1_choices)]
+  #   game_tree.each do |louis_move, p1_responses|
+  #     p1_responses.sort_by! do |response|
+  #       evaluate_board(response.last, LOUIS)
+  #     end
+  #     #select p1's best response
+  #     game_tree[louis_move] = evaluate_board(p1_responses.first.last, LOUIS)
+  #   end
+  #
+  #
+  #   best_choices = game_tree.select do |louis_move, outcome_value|
+  #     outcome_value == game_tree.values.max
+  #   end
+  #   best_choices.keys.sample
+  # end
 
   def choices_array(board_state, player)
     board = Board.new(state: board_state, player: player)

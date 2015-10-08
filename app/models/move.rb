11c01776b3603ab6afa9c6e6605f3a2d61a8575b
nigelfloe <dwr229@gmail.com>
2015-10-08 15:04:43 -0400

    one level minmax

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,5 +1,7 @@
 class Move
   attr_accessor :board, :moves, :start_x, :start_y, :finish_x, :finish_y, :piece_type, :board_state
+  P1 = 0
+  LOUIS = 1
   P1_GUARD_ROW = 7
   LOUIS_GUARD_ROW = 0
   OFF_SQUARE = "-1"
@@ -21,7 +23,9 @@ class Move
   end
 
   def execute
-    if finish_y == P1_GUARD_ROW
+    if board.player == P1 && finish_y == LOUIS_GUARD_ROW
+      @piece_type = P1_KING
+    elsif board.player == LOUIS && finish_y == P1_GUARD_ROW
       @piece_type = LOUIS_KING
     end
     @board_state[finish_y][finish_x] = piece_type

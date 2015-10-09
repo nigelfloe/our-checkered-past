b2e188ec7da1b73b0d814186b848f26d0d92df83
nancyhawa <hawa.nancy@gmail.com>
2015-10-09 13:46:59 -0400

    login issues

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,9 +5,18 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: root_path)
+    else
+      flash[:alert] = "We have no record of that e-mail address."
+      redirect_to root_path
+
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   resources :boards, only: [:create]
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
 
 
   root 'home#index' #landing page/sign up
 
   get '/game', to: 'game#index'
-  # get '/game/new', to: 'game#new'
-  # post '/game', to: 'game#create'
 
   get '/leaderboard', to: 'user#index'
 

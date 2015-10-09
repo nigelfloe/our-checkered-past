bce6854a9cb87ef6d363d5b1f998d7dc0342cf5e
nigelfloe <dwr229@gmail.com>
2015-10-09 13:30:43 -0400

    create win and loss modals

--- app/assets/javascripts/move_cache.js
+++ app/assets/javascripts/move_cache.js
@@ -18,6 +18,25 @@ function tjBoard(){
   board.getBoardState();
 }
 
+function winBoard(){
+  var boardArr = [["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"], ["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"], ["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"], ["-1", "2", "-1", "0", "-1", "0", "-1", "0"], ["1", "-1", "0", "-1", "0", "-1", "0", "-1"]]
+  Board.current().turn.player = Player.all[0];
+  Board.current().player = Player.all[0];
+  board = new Board(Board.current().turn);
+  Board.displayBoardState(boardArr);
+  board.getBoardState();
+  Player.all[0].takesTurn()
+}
+
+function loseBoard(){
+  var boardArr = [["-1", "2", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "1", "-1", "0", "-1", "0", "-1"], ["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"], ["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"], ["-1", "0", "-1", "0", "-1", "0", "-1", "0"], ["0", "-1", "0", "-1", "0", "-1", "0", "-1"]]
+  Board.current().turn.player = Player.all[1];
+  Board.current().player = Player.all[1];
+  board = new Board(Board.current().turn);
+  Board.displayBoardState(boardArr);
+  board.getBoardState();
+}
+
 function setUpMove(){
   // debugger
   Board.all[Board.all.length - 1].sendToDatabase();
